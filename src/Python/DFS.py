1dc1949b513e82920bb3280cc65b63a54ccf2ccc
codeYann <codeyan10@gmail.com>
2021-07-10 12:21:27 -0300

    DFS algorithm

--- src/Python/DFS.py
+++ src/Python/DFS.py
@@ -2,13 +2,14 @@ from listaAdjacencia import Grafo
 
 def DFS_VISIT(grafo, vertice, setVertices):
     setVertices.add(vertice)
-    print(vertice)
+    print("Vertices visitados: {}".format(vertice))
     for v in grafo[vertice]:
         if v not in setVertices:
             DFS_VISIT(grafo, v, setVertices)
 
-def DFS(grafo):
+def DFS(G):
     verticesVizitados = set()
-    for vertices in grafo.keys():
+    for vertices in G.grafo.keys():
         if vertices not in verticesVizitados:
-            DFS_VISIT(grafo, vertices, verticesVizitados)
+            DFS_VISIT(G.grafo, vertices, verticesVizitados)
+    return verticesVizitados
